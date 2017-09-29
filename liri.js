06bbe9d96fdbdb12cd6e3e126c5e4b9b1ccabca4
bindu7772 <manubpatel4@gmail.com>
2017-09-29 12:57:17 -0400

    remove comments

--- liri.js
+++ liri.js
@@ -19,11 +19,3 @@ if (args[0] === "movie-this") {
         }
     });
 }
-//Title of the movie.
-//  * Year the movie came out.
-//  * IMDB Rating of the movie.
-//  * Rotten Tomatoes Rating of the movie.
-//  * Country where the movie was produced.
-//  * Language of the movie.
-//  * Plot of the movie.
-//  * Actors in the movie.
